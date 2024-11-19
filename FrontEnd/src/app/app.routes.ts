ea6d720b026a9afa166214a0f9c883a9c9d1ac19
Gabriel Fonseca <93004749+gabrielfonsecadev@users.noreply.github.com>
2024-11-18 22:16:23 -0300

    Update app.routes.ts

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 
 export const routes: Routes = [
-  {path: 'https://gabrielfonsecadev.github.io/MisterDasPratas/', component: HomeComponent},
+  {path: '', component: HomeComponent},
 ];
