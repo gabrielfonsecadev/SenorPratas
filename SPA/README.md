9fd63b4bbd95900db988b81d6e8e021eb7a70672
Gabriel Fonseca <gabrielfonseca.dsf@gmail.com>
2025-02-13 22:43:10 -0300

    Rename folders

--- SPA/README.md
+++ SPA/README.md
@@ -1,4 +1,4 @@
-# MisterDasPratas2
+# SenorPratas
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.11.
 
--- SPA/src/app/app.component.spec.ts
+++ SPA/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'MisterDasPratas2' title`, () => {
+  it(`should have the 'SenorPratas' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MisterDasPratas2');
+    expect(app.title).toEqual('SenorPratas');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, MisterDasPratas2');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, SenorPratas');
   });
 });
